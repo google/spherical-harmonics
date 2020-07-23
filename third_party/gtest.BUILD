google/spherical-harmonics

commit f3297f7b2c83404f4e6cbc2849680f64ee4bb409

-Upgrading build files to latest Bazel version (3.4.1) -Addding define for windows plattform needed by MSVC

third_party/gtest.BUILD
@@ -1,19 +1,18 @@
-# Base directory for project after unpacking
-googletest_project_dir = "googletest-release-1.8.0/googletest"
+package(
+    default_visibility = ["//visibility:public"],
+)
 
 cc_library(
     name = "main",
     srcs = glob(
-        [googletest_project_dir + "/src/*.h",
-         googletest_project_dir + "/include/gtest/internal/**/*.h",
-         googletest_project_dir + "/src/*.cc"],
-        exclude = [googletest_project_dir + "/src/gtest-all.cc"]
+        include = ["googletest/src/*.h",
+                   "googletest/include/gtest/internal/**/*.h",
+                   "googletest/src/*.cc"],
+        exclude = ["googletest/src/gtest-all.cc"]
     ),
-    hdrs = glob([googletest_project_dir + "/include/gtest/*.h"]),
-    includes = [
-        googletest_project_dir + "/",
-        googletest_project_dir + "/include"
+    hdrs = glob(["googletest/include/gtest/*.h"]),
+    includes = ["/",
+                "/include"
     ],
     linkopts = ["-pthread"],
-    visibility = ["//visibility:public"],
 )

WORKSPACE
@@ -1,23 +1,19 @@
-new_http_archive(
-  name = "http_eigen3",
-  url = "https://bitbucket.org/eigen/eigen/get/3.3.5.zip",
-  sha256 = "35fa84bc23114b9d37c4597745f8b4e03354a5077579fdba597019f595a602b6",
-  build_file = "third_party/eigen3.BUILD",
-)
+workspace(name = "spherical_harmonics")
 
-new_http_archive(
-  name = "http_gtest",
-  url = "https://github.com/google/googletest/archive/release-1.8.0.zip",
-  sha256 = "f3ed3b58511efd272eb074a3a6d6fb79d7c2e6a0e374323d1e6bcbcc1ef141bf",
-  build_file = "third_party/gtest.BUILD",
-)
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
-bind(
+http_archive(
   name = "eigen3",
-  actual = "@http_eigen3//:eigen3",
+  url = "https://bitbucket.org/eigen/eigen/get/3.3.5.zip",
+  strip_prefix = "eigen-eigen-b3f3d4950030",
+  sha256 = "35fa84bc23114b9d37c4597745f8b4e03354a5077579fdba597019f595a602b6",
+  build_file = "@//third_party:eigen3.BUILD",
 )
 
-bind(
+http_archive(
   name = "gtest",
-  actual = "@http_gtest//:main",
+  url = "https://github.com/google/googletest/archive/release-1.8.0.zip",
+  strip_prefix = "googletest-release-1.8.0",
+  sha256 = "f3ed3b58511efd272eb074a3a6d6fb79d7c2e6a0e374323d1e6bcbcc1ef141bf",
+  build_file = "@//third_party:gtest.BUILD",
 )

sh/BUILD
@@ -1,8 +1,15 @@
+config_setting(
+    name = "windows",
+    constraint_values = [
+        "@bazel_tools//platforms:windows",
+    ],
+)
+
 cc_library(
     name = "image",
     hdrs = ["image.h"],
     deps = [
-        "//external:eigen3",
+        "@eigen3//:eigen3",
     ],
     visibility = ["//visibility:public"],
     alwayslink = 1
@@ -14,8 +21,11 @@ cc_library(
     hdrs = ["spherical_harmonics.h"],
     deps = [
         ":image",
-        "//external:eigen3",
+        "@eigen3//:eigen3",
     ],
+    defines = select({
+        ":windows": ["_USE_MATH_DEFINES",],
+    }),
     visibility = ["//visibility:public"],
 )
 
@@ -37,7 +47,7 @@ cc_test(
     deps = [
         ":default_image",
         ":spherical_harmonics",
-        "//external:gtest",
+        "@gtest//:main",
     ],
     linkopts = ["-lm"],
 )

third_party/eigen3.BUILD
@@ -1,23 +1,13 @@
-licenses(["restricted"])  # MPL2, portions GPL v3, LGPL v3, BSD-like
-
-exports_files(["LICENSE"])
-
-# Name of the eigen3 source after being unpacked from its zip
-eigen_project_dir = "eigen-eigen-b3f3d4950030/"
-
-# All Eigen files, including those under a more restrictive license.
-eigen_header_files = glob(
-    include = [eigen_project_dir + "Eigen/**"],
-    exclude = [eigen_project_dir + "Eigen/**/CMakeLists.txt"],
+package(
+    default_visibility = ["//visibility:public"],
 )
 
 cc_library(
     name = "eigen3",
-    hdrs = eigen_header_files,
-    defines = ["EIGEN_MPL2_ONLY"],
-    includes = [eigen_project_dir],
-    licenses = ["notice"],
-    visibility = ["//visibility:public"],
+    hdrs = glob(
+        include = ["Eigen/**"],
+        exclude = ["Eigen/**/CMakeLists.txt"],
+    ),
+    defines = ["EIGEN_MPL2_ONLY", "EIGEN_NO_DEBUG"],
     alwayslink = 1,
 )
-

commit e9174f151f49f316c4cebb6d5dc81fb55ea7164a

Previous commit f3297f7b2c83404f4e6cbc2849680f64ee4bb409 (upgrade to bazel 3.4.1) broke tests. This fixes them.

third_party/gtest.BUILD
@@ -7,9 +7,8 @@ cc_library(
         exclude = ["googletest/src/gtest-all.cc"]
     ),
     hdrs = glob(["googletest/include/gtest/*.h"]),
-    includes = ["/",
-                "/include"
-    ],
+    includes = ["googletest/",
+                "googletest/include"],
     linkopts = ["-pthread"],
     visibility = ["//visibility:public"],
 )